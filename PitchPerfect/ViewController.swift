b15f388294c667e0a72df0f2edf1e0ceaf05575c
Matthias Buehl <buehl@sequoiacap.com>
2020-08-25 18:48:51 -0700

    Initial Commit

---
+++ PitchPerfect/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PitchPerfect
+//
+//  Created by Matthias Buehl on 8/25/20.
+//  Copyright © 2020 Matthias Buehl. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
